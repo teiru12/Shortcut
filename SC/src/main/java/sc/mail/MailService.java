44be02ffe12519c57861d91e93cdd07745554977
jonggyun <pacific1212@naver.com>
2022-03-30 02:16:16 +0900

    병오씨 코드 추가

--- SC/src/main/java/sc/mail/MailService.java
+++ SC/src/main/java/sc/mail/MailService.java
@@ -37,4 +37,26 @@ public class MailService {
             e.printStackTrace();
         }
     }
+    
+    public void sendAuthMail2(String email,String password) {
+        //인증메일 보내기
+        try {
+            MailUtils sendMail = new MailUtils(mailSender);
+            sendMail.setSubject("ShortCut 이메일인증");
+            sendMail.setText(new StringBuffer().append("<h1>[이메일 인증]</h1>")
+            .append("<p>아래 비밀번호로 로그인후 비밀번호 변경하시길 권장드립니다.</p>")
+            .append(password)
+            .append("<p><a href='http://localhost:9003/SC/testFindPw.cut?EMAIL=")
+            .append(email)
+            .append("'>링크</a></p>")
+            .toString());
+            sendMail.setFrom("ShortCut0328@gmail.com", "관리자");
+            sendMail.setTo(email);
+            sendMail.send();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 }
