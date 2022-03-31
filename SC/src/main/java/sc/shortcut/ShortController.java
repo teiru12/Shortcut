00023bf7241d9339472d1e95e1a4533f3d35d549
82109 <82109@DESKTOP-2DKUPIR>
2022-03-31 17:00:33 +0900

    단축키게시판 jsp 추가(이름오류)

--- SC/src/main/java/sc/shortcut/ShortController.java
+++ SC/src/main/java/sc/shortcut/ShortController.java
@@ -1,5 +1,26 @@
 package sc.shortcut;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sc.notice.NoticeService;
+
+@Controller
 public class ShortController {
+	
+	@Resource(name = "shortService")
+	private ShortService shortService;
+	
+	@Resource(name = "noticeService")
+	private NoticeService noticeService;
+	
+	@RequestMapping(value = "/shortcutList.cut")
+	public String shortcutList(Model model) throws Exception {
+		
+		return "shortcutList";
+	}
 
 }
